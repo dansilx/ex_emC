fe92c8d882491254108bf4a92d7d9763656798fa
dansilx <danilosilveira@msn.com>
2024-01-31 01:59:38 -0300

    comeco dos estudos em C

---
+++ ex002.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    tipo nome;
+    char 
+    int
+    float 
+    double
+    //NOMEANDO VARIÁVEIS
+    a, b, c, d,
+    A, B, C, D, 
+    _
+
+    //linguagem CASE SENSITIVE
+    soma
+    SOMA
+    Soma
+    SomA
+
+    //declarando variavel
+
+    char letra = 'a';
+    char nro = 10;
+    int nro = 10;
+    float n = 5.25:
+    double d = 5.0e3;
+    int nro1 = 10;
+    int nro2;
+    int nro3; 
+    int nro1= 10, nro2, nro3; 
+    //é possivel declarar as variáveis todas juntas quando se encaixam no mesmo tipo.
+
+    system("pause");
+}
